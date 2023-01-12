25aa5ed899bc30cf5598f51147a42b6e5a887933
whl <wplovitz@gmail.com>
2023-01-11 19:00:00 -0800

    added Form structure for signup

--- client/src/components/pages/SignupForm.js
+++ client/src/components/pages/SignupForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEfect, useEffect } from 'react'
-import { From, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../../utils/mutations';
 import Auth from  '../../utils/auth';
@@ -33,8 +33,78 @@ const SignupForm = () => {
         if (form.checkValidity() === false ) {
             event.preventDefault();
             event.stopPropagation();
-        } 
-    }
+        }
+        
+        try {
+            const { data } = await addUser({
+                variables: { ...userFormData },
+            });
+            console.log(data);
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.error(err);
+        }
 
+        setUserFormData({
+            username:'',
+            email:'',
+            password:'',
+        });
+    };
 
-}
+    return (
+        <>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+                Something went wrong with your signup!
+            </Alert>
+
+            <Form.Group>
+                <Form.Label htmlFor='username'> Username</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder='Your username'
+                name='usename'
+                onChange={handleInputChange}
+                value={userFormData.username}
+                required
+                />
+                <Form.Control.Feedback type='invalid'> Username is required</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group> 
+                <Form.Label htmlFor="email">Email</Form.Label>
+                <Form.Control
+                type='email'
+                placeholder='your Email'
+                name='email'
+                onChange={handleInputChange}
+                value={userFormData.email}
+                required
+                />
+                <Form.Control.Feedback type='invalid'> Email is required</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label htmlFor='password'>Password</Form.Label>
+                <Form.Control
+                type="password"
+                placeholder='Your password'
+                name='password'
+                onChange={handleInputChange}
+                value={userFormData.password}
+                required
+                />
+                <Form.Control.Feedback type='invalid'> Password is required!</Form.Control.Feedback>
+            </Form.Group>
+            <Button
+            disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+            type='submit'
+            variant='success'>
+                submit
+            </Button>
+        </Form>
+        </>
+    );
+};
+
+export default SignupForm;
