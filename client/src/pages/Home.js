138e56b65affbe1c5b840bc1501cb3a271c1a09c
Christian Flores <c1flores@ucsd.edu>
2023-01-18 22:00:37 -0800

    major-refactoring pt2

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+
 
 const Home = () => {
   return (
