a547bc92b7eaa4eede6512fb7fb2eadafe75ce36
Christian Flores <c1flores@ucsd.edu>
2023-01-21 16:28:40 -0800

    Add pastry products to website

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -41,9 +41,6 @@ function ProductItem(item) {
         <p>{name}</p>
       </Link>
       <div>
-        <div>
-          {quantity} {pluralize('item', quantity)} in stock
-        </div>
         <span>${price}</span>
       </div>
       <button onClick={addToCart}>Add to cart</button>
