675f5fa12ae32918158fd92342f766bc9c486f03
whl <wplovitz@gmail.com>
2023-01-22 15:39:25 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
-process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deja-brewdb';
+process.env.MONGODB_URI || 'mongodb://locolhost/deja-brewdb';
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect(connectionString, {
