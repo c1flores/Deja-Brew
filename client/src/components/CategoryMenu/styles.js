896f7493526e8406321f277129627176ab09cb05
whl <wplovitz@gmail.com>
2023-01-22 20:15:11 -0800

    changed nav bar color and product button color

--- client/src/components/CategoryMenu/styles.js
+++ client/src/components/CategoryMenu/styles.js
@@ -2,6 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   buttons: {
-    margin: '10px'
+    margin: '10px',
+    backgroundColor: '#4a2c2a',
+    color: 'white',
   },
 }));
